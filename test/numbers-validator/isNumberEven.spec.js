178538923ebed3ae067446c101b31437419668ba
Pablo Salcedo <pablo.salcedom@gmail.com>
2025-06-02 20:03:30 -0600

    preparing test

---
+++ test/numbers-validator/isNumberEven.spec.js
@@ -0,0 +1,12 @@
+// Importing the 'expect' function from the 'chai' library to perform assertions
+const { expect } = require('chai');
+
+//Importing the NumbersValidator class from the relative path '../app/numbers-validator'
+// so we can test its methods.
+const NumbersValidator = require('../../app/numbers_validator');
+
+// 'descripe' is used to group related tests together into a test suite. 
+// Here, it describes a suite of tests for the 'isNumberEven' method of NumbersValidator class.
+describe('isNumberEven', () => {
+    
+})
